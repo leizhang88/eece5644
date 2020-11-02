bd92a4d7fa743d04f52a6c79298edce152402c05
Lei Zhang <callmelei@outlook.com>
2020-11-01 20:38:57 -0500

    Change figure save directory in 'HW1Q1.py'

--- HW1Q1.py
+++ HW1Q1.py
@@ -21,7 +21,7 @@ C = np.array([[[2, -.5, .3, 0],
                [.3, -.5, 1, 0],
                [0, 0, 0, 2]],
 
-               [[1, .3, -.2, 0],
+              [[1, .3, -.2, 0],
                [.3, 2, .3, 0],
                [-.2, .3, 1, 0],
                [0, 0, 0, 3]]])
@@ -42,8 +42,8 @@ temp = np.insert(np.cumsum(class_prior), 0, 0)
 for i in range(NC):
     ind = (rnd >= temp[i]) & (rnd < temp[i + 1])
     class_sample_num[i] = np.sum(ind)
-    X[:, ind] = random.multivariate_normal(m[i, :, :].flatten(), 
-     C[i, :, :], class_sample_num[i]).T
+    X[:, ind] = random.multivariate_normal(m[i, :, :].flatten(),
+                                           C[i, :, :], class_sample_num[i]).T
 
 # ----------------------------------------------
 # Part A: ERM classification with true class pdf
@@ -52,11 +52,11 @@ discriminant_score = np.zeros(N)
 
 # likelihood-ratio
 for i in range(N):
-    discriminant_score[i] = np.log(multivariate_normal.pdf(X[:, i], 
-                      mean=m[1, :, :].flatten(), cov=C[1, :, :]) / \
-                      multivariate_normal.pdf(X[:, i], 
-                                              mean=m[0, :, :].flatten(), 
-                                              cov=C[0, :, :]))
+    discriminant_score[i] = np.log(multivariate_normal.pdf(X[:, i],
+                                                           mean=m[1, :, :].flatten(), cov=C[1, :, :]) /
+                                   multivariate_normal.pdf(X[:, i],
+                                                           mean=m[0, :, :].flatten(),
+                                                           cov=C[0, :, :]))
 
 # choose threshold as the midpoint of every two sorted discriminant score
 score_sort = np.sort(discriminant_score)
@@ -82,8 +82,8 @@ threshold_best = threshold[np.argmin(Perr_a)]
 fig1 = plt.figure(figsize=[4, 3], dpi=150)
 ax1 = fig1.add_subplot(111)
 ax1.plot(P10, P11, linewidth=1)
-ax1.scatter(P10[np.argmin(Perr_a)], P11[np.argmin(Perr_a)], c='r', 
-                marker='x', label=r'minimum $P_{error}$')
+ax1.scatter(P10[np.argmin(Perr_a)], P11[np.argmin(Perr_a)], c='r',
+            marker='x', label=r'minimum $P_{error}$')
 ax1.set_xlim([0, 1])
 ax1.set_ylim([0, 1])
 ax1.set_xlabel(r'$P_{FP}$')
@@ -91,24 +91,23 @@ ax1.set_ylabel(r'$P_{TP}$')
 ax1.set_title('ROC curve with ture class pdf')
 ax1.legend()
 plt.tight_layout()
-plt.savefig('Q1A_roc.pdf')
+plt.savefig('../hw1/Q1A_roc.pdf')
 plt.show()
 
 # plot P_error
 fig2 = plt.figure(figsize=[4, 3], dpi=150)
 ax2 = fig2.add_subplot(111)
 ax2.plot(threshold, Perr_a, linewidth=1)
-plt.annotate(f'{min(Perr_a):5.2%}', fontsize =6, 
-                xy=(threshold_best - .5, min(Perr_a)),
-                xytext=(threshold_best - 15, min(Perr_a)), 
-                arrowprops=dict(arrowstyle='->'))
+plt.annotate(f'{min(Perr_a):5.2%}', fontsize=6,
+             xy=(threshold_best - .5, min(Perr_a)),
+             xytext=(threshold_best - 15, min(Perr_a)),
+             arrowprops=dict(arrowstyle='->'))
 ax2.set_xlabel(r'threshold')
 ax2.set_ylabel(r'$P_{error}$')
 ax2.set_title(r'$P_{error}$ with true class pdf')
 plt.tight_layout()
-plt.savefig('Q1A_perror.pdf')
+plt.savefig('../hw1/Q1A_perror.pdf')
 plt.show()
-
 
 # --------------------------------------------------
 # Part B: ERM classification with mismatch class pdf
@@ -117,12 +116,12 @@ discriminant_score = np.zeros(N)
 
 # likelihood-ratio, change true covariance with identity matrix
 for i in range(N):
-    discriminant_score[i] = np.log(multivariate_normal.pdf(X[:, i], 
-                      mean=m[1, :, :].flatten(), 
-                      cov=np.diag(np.diag(C[1, :, :]))) / \
-                      multivariate_normal.pdf(X[:, i], 
-                      mean=m[0, :, :].flatten(), 
-                      cov=np.diag(np.diag(C[0, :, :]))))
+    discriminant_score[i] = np.log(multivariate_normal.pdf(X[:, i],
+                                                           mean=m[1, :, :].flatten(),
+                                                           cov=np.diag(np.diag(C[1, :, :]))) /
+                                   multivariate_normal.pdf(X[:, i],
+                                                           mean=m[0, :, :].flatten(),
+                                                           cov=np.diag(np.diag(C[0, :, :]))))
 
 # choose threshold as the midpoint of every two sorted discriminant score
 score_sort = np.sort(discriminant_score)
@@ -148,8 +147,8 @@ threshold_best = threshold_b[np.argmin(Perr_b)]
 fig1 = plt.figure(figsize=[4, 3], dpi=150)
 ax1 = fig1.add_subplot(111)
 ax1.plot(P10_b, P11_b, linewidth=1)
-ax1.scatter(P10_b[np.argmin(Perr_b)], P11_b[np.argmin(Perr_b)], c='r', 
-                marker='x', label=r'minimum $P_{error}$')
+ax1.scatter(P10_b[np.argmin(Perr_b)], P11_b[np.argmin(Perr_b)], c='r',
+            marker='x', label=r'minimum $P_{error}$')
 ax1.set_xlim([0, 1])
 ax1.set_ylim([0, 1])
 ax1.set_xlabel(r'$P_{FP}$')
@@ -157,28 +156,27 @@ ax1.set_ylabel(r'$P_{TP}$')
 ax1.set_title('ROC curve with mismatch class pdf')
 ax1.legend()
 plt.tight_layout()
-plt.savefig('Q1B_roc.pdf')
+plt.savefig('../hw1/Q1B_roc.pdf')
 plt.show()
 
 # plot P_error
 fig2 = plt.figure(figsize=[4, 3], dpi=150)
 ax2 = fig2.add_subplot(111)
 ax2.plot(threshold_b, Perr_b, linewidth=1)
-plt.annotate(f'{min(Perr_b):5.2%}', fontsize =6, xy=(threshold_best-.5, 
-                min(Perr_b)),
-                xytext=(threshold_best-10, min(Perr_b)), 
-                arrowprops=dict(arrowstyle='->'))
+plt.annotate(f'{min(Perr_b):5.2%}', fontsize=6, xy=(threshold_best - .5,
+                                                    min(Perr_b)),
+             xytext=(threshold_best - 10, min(Perr_b)),
+             arrowprops=dict(arrowstyle='->'))
 ax2.set_xlabel(r'threshold')
 ax2.set_ylabel(r'$P_{error}$')
 ax2.set_title(r'$P_{error}$ with mismatch class pdf')
 plt.tight_layout()
-plt.savefig('Q1B_perror.pdf')
+plt.savefig('../hw1/Q1B_perror.pdf')
 plt.show()
 
-
-#----------------------------------
+# ----------------------------------
 # Part C: Fisher LDA classification
-#----------------------------------
+# ----------------------------------
 # class conditional pdf mean and covariance
 muhat0 = np.mean(X[:, ~y], axis=1)
 sighat0 = np.cov(X[:, ~y])
@@ -203,7 +201,7 @@ score_sort = np.sort(discriminant_score_LDA).ravel()
 threshold_LDA = (score_sort[0:-1] + score_sort[1:]) / 2
 threshold_LDA = np.insert(threshold_LDA, 0, score_sort[0] - eps)
 threshold_LDA = np.append(threshold_LDA, score_sort[-1] + eps)
-    
+
 P01_c = np.zeros(threshold_LDA.size)
 P10_c = np.zeros(threshold_LDA.size)
 P11_c = np.zeros(threshold_LDA.size)
@@ -219,8 +217,8 @@ for i in range(threshold_LDA.size):
 fig1 = plt.figure(figsize=[4, 3], dpi=150)
 ax1 = fig1.add_subplot(111)
 ax1.plot(P10_c, P11_c, linewidth=1)
-ax1.scatter(P10_c[np.argmin(Perr_c)], P11_c[np.argmin(Perr_c)], c='r', 
-                marker='x', label=r'minimum $P_{error}$')
+ax1.scatter(P10_c[np.argmin(Perr_c)], P11_c[np.argmin(Perr_c)], c='r',
+            marker='x', label=r'minimum $P_{error}$')
 ax1.set_xlim([0, 1])
 ax1.set_ylim([0, 1])
 ax1.set_xlabel(r'$P_{FP}$')
@@ -228,32 +226,32 @@ ax1.set_ylabel(r'$P_{TP}$')
 ax1.set_title('ROC curve by Fisher LDA classifer')
 ax1.legend()
 plt.tight_layout()
-plt.savefig('Q1C_roc.pdf')
+plt.savefig('../hw1/Q1C_roc.pdf')
 plt.show()
 
 # plot P_error
 fig2 = plt.figure(figsize=[4, 3], dpi=150)
 ax2 = fig2.add_subplot(111)
 ax2.plot(threshold_LDA, Perr_c, linewidth=1)
-plt.annotate(f'{min(Perr_c):4.2%}', fontsize =6, 
-                xy=(threshold_LDA[np.argmin(Perr_c)] - .1, 
-                min(Perr_c)),
-                xytext=(threshold_LDA[np.argmin(Perr_c)] - 2, min(Perr_c)), 
-                arrowprops=dict(arrowstyle='->'))
+plt.annotate(f'{min(Perr_c):4.2%}', fontsize=6,
+             xy=(threshold_LDA[np.argmin(Perr_c)] - .1,
+                 min(Perr_c)),
+             xytext=(threshold_LDA[np.argmin(Perr_c)] - 2, min(Perr_c)),
+             arrowprops=dict(arrowstyle='->'))
 ax2.set_xlabel(r'threshold')
 ax2.set_ylabel(r'$P_{error}$')
 ax2.set_title(r'$P_{error}$ by Fisher LDA classifer')
 plt.tight_layout()
-plt.savefig('Q1C_perror.pdf')
+plt.savefig('../hw1/Q1C_perror.pdf')
 plt.show()
 
-#---------------------------------------------------------------
+# ---------------------------------------------------------------
 # Comparisons of ROC curves and P_error between these approaches
-#---------------------------------------------------------------
+# ---------------------------------------------------------------
 fig3 = plt.figure(figsize=[4, 3], dpi=150)
 ax3 = fig3.gca()
 ax3.plot(P10, P11, linewidth=1, linestyle='-', c='m', label='ERM true pdf')
-ax3.plot(P10_b, P11_b, linewidth=1, linestyle='--', c='g', 
+ax3.plot(P10_b, P11_b, linewidth=1, linestyle='--', c='g',
          label='ERM mismatch pdf')
 ax3.plot(P10_c, P11_c, linewidth=1, linestyle=':', c='b', label='LDA')
 ax3.set_xlim([0, 1])
@@ -263,23 +261,23 @@ ax3.set_ylabel(r'$P_{TP}$')
 ax3.set_title('ROC curve comparison')
 ax3.legend(fontsize=8)
 plt.tight_layout()
-plt.savefig('Q1_roccmp1.pdf')
+plt.savefig('../hw1/Q1_roccmp1.pdf')
 plt.show()
 
 fig4 = plt.figure(figsize=[4, 3], dpi=150)
 ax4 = fig4.gca()
-ax4.plot(threshold, Perr_a, linewidth=1, linestyle='-', c='m', 
+ax4.plot(threshold, Perr_a, linewidth=1, linestyle='-', c='m',
          label='ERM true pdf')
-ax4.plot(threshold_b, Perr_b, linewidth=1, linestyle='--', c='g', 
+ax4.plot(threshold_b, Perr_b, linewidth=1, linestyle='--', c='g',
          label='ERM mismatch pdf')
-ax4.plot(threshold_LDA, Perr_c, linewidth=1, linestyle=':', c='b', 
+ax4.plot(threshold_LDA, Perr_c, linewidth=1, linestyle=':', c='b',
          label='LDA')
-#ax4.set_ylim([0, .1])
-#ax4.set_xlim([-5, 5])
+# ax4.set_ylim([0, .1])
+# ax4.set_xlim([-5, 5])
 ax4.set_xlabel(r'threshold')
 ax4.set_ylabel(r'$P_{error}$')
 ax4.set_title(r'$P_{error}$ comparison')
 ax4.legend(fontsize=8)
 plt.tight_layout()
-plt.savefig('Q1_perrcmp1.pdf')
+plt.savefig('../hw1/Q1_perrcmp1.pdf')
 plt.show()
